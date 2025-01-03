72d4121848fee2026d1e5b4dc55639d2976015f5
mnishanthkumar12 <mnishanthkumar9@gmail.com>
2025-01-03 19:38:54 +0530

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Login from './Login';
-// import Register from './Register';
-// import ThankYou from './ThankYou';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/thank-you" element={<ThankYou />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
