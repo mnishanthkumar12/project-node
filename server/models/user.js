139053852c98db505dd5f3c58252473273db3eec
mnishanthkumar12 <mnishanthkumar9@gmail.com>
2025-01-03 19:45:04 +0530

    user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,39 +1,3 @@
-// const db = require('./db');
-
-// // Register new user
-// const createUser = async (userData) => {
-//     const query = `INSERT INTO users (name, email, password, company_name, age, dob, image) 
-//                    VALUES (?, ?, ?, ?, ?, ?, ?)`;
-//     const [result] = await db.execute(query, [
-//         userData.name,
-//         userData.email,
-//         userData.password,
-//         userData.company_name,
-//         userData.age,
-//         userData.dob,
-//         userData.image, // Image URL or File Path after uploading
-//     ]);
-//     return result;
-// };
-
-// // Find user by email
-// const findUserByEmail = async (email) => {
-//     const query = `SELECT * FROM users WHERE email = ?`;
-//     const [rows] = await db.execute(query, [email]);
-//     return rows[0];
-// };
-
-// // Delete user by email
-// const deleteUserByEmail = async (email) => {
-//     const query = `DELETE FROM users WHERE email = ?`;
-//     const [result] = await db.execute(query, [email]);
-//     return result;
-// };
-
-// module.exports = { createUser, findUserByEmail, deleteUserByEmail };
-
-
-
 const db = require('./db');
 
 const createUser = async (userData) => {
@@ -58,21 +22,16 @@ const createUser = async (userData) => {
   };
   
 
-// Find user by email
 const findUserByEmail = async (email) => {
     const query = `SELECT * FROM users WHERE email = ?`;
     const [rows] = await db.execute(query, [email]);
     return rows[0];
 };
-
-// Update OTP and OTP expiry for a user
 const updateOtp = async (email, otp, otpExpiryTime) => {
     const query = `UPDATE users SET otp = ?, otp_expiry = ? WHERE email = ?`;
     const [result] = await db.execute(query, [otp, otpExpiryTime, email]);
     return result;
   };
-
-// Delete user by email (assuming you are using a MySQL database)
 const deleteUserByEmail = async (email) => {
     try {
       const query = 'DELETE FROM users WHERE email = ?';
@@ -88,88 +47,3 @@ const deleteUserByEmail = async (email) => {
 module.exports = { createUser, findUserByEmail, deleteUserByEmail, updateOtp };
 
 
-
-// const db = require('./db');
-
-// // Register new user
-// const createUser = async (userData) => {
-//     const query = `INSERT INTO users (name, email, password, company_name, age, dob, image) 
-//                    VALUES (?, ?, ?, ?, ?, ?, ?)`;
-//     const [result] = await db.execute(query, [
-//         userData.name,
-//         userData.email,
-//         userData.password,
-//         userData.company_name,
-//         userData.age,
-//         userData.dob,
-//         userData.image, // Image URL or File Path after uploading
-//     ]);
-//     return result;
-// };
-
-// // Find user by email
-// const findUserByEmail = async (email) => {
-//     const query = `SELECT * FROM users WHERE email = ?`;
-//     const [rows] = await db.execute(query, [email]);
-//     return rows[0];
-// };
-
-// // Update OTP and OTP expiry for a user
-// const updateOtp = async (email, otp, otpExpiryTime) => {
-//     const query = `UPDATE users SET otp = ?, otp_expiry = ? WHERE email = ?`;
-//     const [result] = await db.execute(query, [otp, otpExpiryTime, email]);
-//     return result;
-// };
-
-// // Delete user by email
-// const deleteUserByEmail = async (email) => {
-//     const query = `DELETE FROM users WHERE email = ?`;
-//     const [result] = await db.execute(query, [email]);
-//     return result;
-// };
-
-// module.exports = { createUser, findUserByEmail, deleteUserByEmail, updateOtp };
-
-
-
-
-// const db = require('./db');
-
-// // Register new user
-// const createUser = async (userData) => {
-//     const query = `INSERT INTO users (name, email, password, company_name, age, dob, image) 
-//                    VALUES (?, ?, ?, ?, ?, ?, ?)`;
-//     const [result] = await db.execute(query, [
-//         userData.name,
-//         userData.email,
-//         userData.password,
-//         userData.company_name,
-//         userData.age,
-//         userData.dob,
-//         userData.image, // Image URL or File Path after uploading
-//     ]);
-//     return result;
-// };
-
-// // Find user by email
-// const findUserByEmail = async (email) => {
-//     const query = `SELECT * FROM users WHERE email = ?`;
-//     const [rows] = await db.execute(query, [email]);
-//     return rows[0];
-// };
-
-// // Update OTP and OTP expiry for a user
-// const updateOtp = async (email, otp, otpExpiryTime) => {
-//     const query = `UPDATE users SET otp = ?, otp_expiry = ? WHERE email = ?`;
-//     const [result] = await db.execute(query, [otp, otpExpiryTime, email]);
-//     return result;
-// };
-
-// // Delete user by email
-// const deleteUserByEmail = async (email) => {
-//     const query = `DELETE FROM users WHERE email = ?`;
-//     const [result] = await db.execute(query, [email]);
-//     return result;
-// };
-
-// module.exports = { createUser, findUserByEmail, deleteUserByEmail, updateOtp };
